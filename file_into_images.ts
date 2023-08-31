4f6dc27a96b9f1a8cebff16888bf25cd8f43ca1a
dong-ju <spdlqj011@gmail.com>
2023-08-31 19:38:53 +0900

    use gzip compress

--- file_into_images.ts
+++ file_into_images.ts
@@ -1,43 +1,31 @@
 import * as fs from "fs";
+import * as zlib from 'zlib';
 import { createCanvas } from "canvas";
 
-// Directory where the files are located
-const directoryPath = "/Users/dong-ju/Documents/My_code/file_images/files/"; // Replace with your actual directory path
-
-// Get list of all files in the directory
+const directoryPath = "/Users/dong-ju/Documents/My_code/file_images/files/";
 const fileNames = fs.readdirSync(directoryPath);
 
-// Loop through each file
 fileNames.forEach((fileName) => {
-    // Complete file path
+    // Ignore .DS_Store
+    if (fileName === '.DS_Store') {
+        return;
+    }
     const filePath = `${directoryPath}/${fileName}`;
-
-    // Read the file into binary
     let binaryData: Buffer = fs.readFileSync(filePath);
-
-    // Target size for a 256x256 RGB image
     const targetSize = 256 * 256 * 3;
 
     if (binaryData.length < targetSize) {
-        // Padding if data is smaller
         const padding = Buffer.alloc(targetSize - binaryData.length);
         binaryData = Buffer.concat([binaryData, padding]);
     } else if (binaryData.length > targetSize) {
-        // Compress if data is larger
-        const step = Math.ceil(binaryData.length / targetSize);
-        let compressedData: number[] = [];
-        for (let i = 0; i < binaryData.length; i += step) {
-            compressedData.push(binaryData[i]);
-        }
-        binaryData = Buffer.from(compressedData);
+        binaryData = zlib.gzipSync(binaryData);
+        // Optionally, you can then slice the compressed data to fit the target size
+        // binaryData = binaryData.slice(0, targetSize);
     }
 
-    // Create a 256x256 RGB image
     const canvas = createCanvas(256, 256);
     const ctx = canvas.getContext("2d");
-
-    let x = 0,
-        y = 0;
+    let x = 0, y = 0;
 
     for (let i = 0; i < targetSize; i += 3) {
         const r = binaryData[i];
@@ -45,7 +33,6 @@ fileNames.forEach((fileName) => {
         const b = binaryData[i + 2];
         ctx.fillStyle = `rgb(${r},${g},${b})`;
         ctx.fillRect(x, y, 1, 1);
-
         x++;
         if (x >= 256) {
             x = 0;
@@ -53,7 +40,6 @@ fileNames.forEach((fileName) => {
         }
     }
 
-    // Save to a file with the same filename but with .png extension
     const outputFileName = `/Users/dong-ju/Documents/My_code/file_images/images/malicious/${fileName
         .split(".")
         .slice(0, -1)
