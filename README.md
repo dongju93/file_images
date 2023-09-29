f7e4e0e059fb565b2f1a30a84da3adf8e314d9e1
dong-ju <spdlqj011@gmail.com>
2023-09-29 19:00:41 +0900

    change image generate method

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # GOAL
+
 1️⃣ Convert the malicious file to a .PNG image - ✅  
 2️⃣ Image size is 256x256, RGB color - ✅  
 3️⃣ Create deep learning CNN model - ✅  
 4️⃣ Feed the model with an image of the suspicious file (steps 1-2) - ✅  
-5️⃣ Derive how similar the result is to the trained (malicious) content in %.  - ⬜️  
-6️⃣ Determine malicious if it is above a certain similarity %, and not malicious if it is below.  - ⬜️  
-7️⃣ (option) Visualize the model training and results  - ⬜️
+5️⃣ Derive how similar the result is to the trained (malicious) content in %. - ⬜️  
+6️⃣ Determine malicious if it is above a certain similarity %, and not malicious if it is below. - ⬜️  
+7️⃣ (option) Visualize the model training and results - ⬜️
 
 < based on this [article](https://ieeexplore.ieee.org/document/8887303) >
 </br></br>
 
+Change file into images
+
+```
+// typescript compile
+// tsc file_into_images.ts
+
+// run javascript
+node file_into_images.js
+```
+
 [Pytorch](https://pytorch.org)
+
 ```
 // for run this code
 // open pytorch_cnn.ipynb and select python version than click Run All
@@ -20,6 +32,7 @@ pip install torch torchvision Pillow matplotlib
 ```
 
 [Tensorflow](https://www.tensorflow.org/)
+
 ```
 // for run this code excute command below
 node tensorflow_cnn.js
@@ -27,6 +40,5 @@ node tensorflow_cnn.js
 // dependencys are required
 npm i @tensorflow/tfjs-node pngjs
 ```
-
 
 #### Copyright 2023. ClumL Inc. all rights reserved
--- file_into_images.ts
+++ file_into_images.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import * as zlib from 'zlib';
+import * as zlib from "zlib";
 import { createCanvas } from "canvas";
 
 const directoryPath = "/Users/dong-ju/Documents/My_code/file_images/files/";
@@ -7,39 +7,42 @@ const fileNames = fs.readdirSync(directoryPath);
 
 fileNames.forEach((fileName) => {
     // Ignore .DS_Store
-    if (fileName === '.DS_Store') {
+    if (fileName === ".DS_Store") {
         return;
     }
+
     const filePath = `${directoryPath}/${fileName}`;
     let binaryData: Buffer = fs.readFileSync(filePath);
     const targetSize = 256 * 256 * 3;
 
+    // If the binary data is smaller than the target size, pad it with zeros
     if (binaryData.length < targetSize) {
         const padding = Buffer.alloc(targetSize - binaryData.length);
         binaryData = Buffer.concat([binaryData, padding]);
     } else if (binaryData.length > targetSize) {
+        // If the binary data is larger than the target size, compress it and then slice it to the target size
         binaryData = zlib.gzipSync(binaryData);
-        // Optionally, you can then slice the compressed data to fit the target size
         binaryData = binaryData.slice(0, targetSize);
     }
 
     const canvas = createCanvas(256, 256);
     const ctx = canvas.getContext("2d");
-    let x = 0, y = 0;
 
+    // Loop through the binary data and set the color of each pixel in the image
     for (let i = 0; i < targetSize; i += 3) {
         const r = binaryData[i];
         const g = binaryData[i + 1];
         const b = binaryData[i + 2];
+
+        // Calculate the x and y coordinates based on the red and green values
+        const x = r % 256;
+        const y = g % 256;
+
         ctx.fillStyle = `rgb(${r},${g},${b})`;
         ctx.fillRect(x, y, 1, 1);
-        x++;
-        if (x >= 256) {
-            x = 0;
-            y++;
-        }
     }
 
+    // Construct the output file name and save the image
     const outputFileName = `/Users/dong-ju/Documents/My_code/file_images/images/malicious/${fileName
         .split(".")
         .slice(0, -1)
